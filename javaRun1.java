9e1bb9e3305abe5bd6b9b2498f0bb541d11ddb85
Eman Hashim <emanhashim42@gmail.com>
2022-11-28 15:31:41 +0300

    day first we studied how we can run java from our computer using javac

---
+++ javaRun1.java
@@ -0,0 +1,14 @@
+public class javaRun1 {
+    public static void main(String [] args){
+        System.out.println("hello there");
+
+    }
+}
+//so this is day first  study on november 28
+//to run from cmd and to check if java installed or not
+/*
+* then write the above code then cd into the folder from cmd
+* then "javac javaRun1", if there is no error it prompts you to the next line
+* after that type "java javaRun1", this show the out put
+* this is how you run projects from cmd and using just "javac"
+* */
